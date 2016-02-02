04fc07e5676dff4cdf785a204cdd75e6a9cce14b
Nikhil Thota <nt92@ufl.edu>
2016-02-01 21:51:28 -0500

    Add initial project

---
+++ TweetApp/TweetApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TweetApp
+//
+//  Created by Nikhil Thota on 2/1/16.
+//  Copyright © 2016 Nikhil Thota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
